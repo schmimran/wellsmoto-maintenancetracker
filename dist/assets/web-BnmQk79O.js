fffd838b2b75b95ccbf737104a65e059319cd574
schmimran <me@schmim.com>
2025-05-16 19:20:57 -0400

    Successful test with build artifacts for iOS and Android

---
+++ dist/assets/web-BnmQk79O.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-N9SQL0ro.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { initializeNativeUI, setupKeyboard } from './services/capacitor';
+import { setupAndroidBackButton, initializeNativeUI, setupKeyboard } from './services/capacitor';
 
 // Initialize native UI when running in native environment
 document.addEventListener('DOMContentLoaded', () => {
