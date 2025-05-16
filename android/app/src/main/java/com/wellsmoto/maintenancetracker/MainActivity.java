ad0f12d5fcc73bf86bfc437e1f5e99758ef2f860
schmimran <me@schmim.com>
2025-05-16 15:04:45 -0400

    First port attempt

---
+++ android/app/src/main/java/com/wellsmoto/maintenancetracker/MainActivity.java
@@ -0,0 +1,5 @@
+package com.wellsmoto.maintenancetracker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,22 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wellsmoto.maintenancetracker',
+  appName: 'WellsMoto Tracker',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    buildOptions: {
+      // Specify the Java version to use
+      minSdkVersion: 22,
+      targetSdkVersion: 33,
+      // Other build options
+      gradleArgs: ['-PcdvMinSdkVersion=22']
+    }
+  }
+};
+
+export default config;
